bf40347750ba0b7540ee5b3839f2ff1d33f524aa
bglimepoint <40051843+bglimepoint@users.noreply.github.com>
2019-07-19 15:30:47 +1000

    Update module path.

--- setup.go
+++ setup.go
@@ -3,7 +3,7 @@ package redis
 import (
 	"strconv"
 
-	"github.com/mholt/caddy"
+	"github.com/caddyserver/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 )
