ae8ebba55579283ae0f66e81bb54591092157156
Andrew Ayoub <prog.andrew2015@yahoo.com>
2019-08-07 14:01:23 +0200

    update caddyserver import

--- setup.go
+++ setup.go
@@ -3,7 +3,7 @@ package redis
 import (
 	"strconv"
 
-	"github.com/caddyserver/caddyserver"
+	"github.com/caddyserver/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
 )
